5c63b894bb171dc25acf599b897b3966b7f8e039
Daniel Pataki <me@danielpataki.com>
2015-07-14 14:59:08 +0200

    First Version

---
+++ acf-madmimi_audience_list/trunk/acf-madmimi_audience_list-v4.php
@@ -0,0 +1,444 @@
+<?php
+
+class acf_field_madmimi_audience_list extends acf_field {
+
+	// vars
+	var $settings, // will hold info such as dir / path
+		$defaults; // will hold default field options
+
+	var $madmimi;
+	var $madmimi_lists;
+
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function __construct()
+	{
+		// vars
+
+		$this->madmimi = new MadMimi(mal_madmimi_username(), mal_madmimi_api_key() );
+		$this->set_madmimi_lists();
+		$this->name = 'madmimi_audience_list';
+		$this->label = __('MadMimi Audience List');
+		$this->category = __("Choice",'acf'); // Basic, Content, Choice, etc
+		$this->defaults = array(
+			// add default here to merge into your field.
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'preview_size' => 'thumbnail'
+		);
+
+
+		// do not delete!
+    	parent::__construct();
+
+
+    	// settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
+
+	}
+
+
+	function set_madmimi_lists() {
+		$list_array = get_transient( 'mal_audience_lists' );
+		if( empty( $list_array ) ) {
+
+			$lists = $this->madmimi->Lists();
+			$lists = new SimpleXMLElement( $lists );
+			$list_array = array();
+			foreach( $lists as $list ) {
+				$list = (array) $list;
+				if( empty( $list['@attributes']['display_name'] ) ) {
+					$list['@attributes']['display_name'] = $list['@attributes']['name'];
+				}
+				$list_array[$list['@attributes']['id']] = $list['@attributes'];
+			}
+
+			set_transient( 'mal_audience_lists', $list_array, HOUR_IN_SECONDS );
+		}
+		$this->madmimi_lists = $list_array;
+	}
+
+
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like below) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+
+	function create_options( $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+
+		// key is needed in the field names to correctly save the data
+		$key = $field['name'];
+
+
+		// Create Field Options HTML
+		?>
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Return Format",'acf-madmimi_audience_list'); ?></label>
+		<p class="description"><?php _e("Set the type of data you would like the field to return",'acf-madmimi_audience_list'); ?></p>
+	</td>
+	<td>
+		<?php
+
+		do_action('acf/create_field', array(
+			'type'		=>	'select',
+			'name'		=>	'fields['.$key.'][return]',
+			'value'		=>	$field['return'],
+			'choices'       => array(
+				'array' => __( 'Array of all API Data', 'acf-madmimi_audience_list' ),
+				'id' => __( 'List ID', 'acf-madmimi_audience_list' ),
+				'name' => __( 'Name', 'acf-madmimi_audience_list' ),
+				'display_name' => __( 'Display Name', 'acf-madmimi_audience_list' ),
+				'subscriber_count' => __( 'Subscrber Count', 'acf-madmimi_audience_list' )
+			)
+		));
+
+		?>
+	</td>
+</tr>
+
+
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Allow Multiple",'acf-madmimi_audience_list'); ?></label>
+	</td>
+	<td>
+		<?php
+
+		do_action('acf/create_field', array(
+			'type'		=>	'true_false',
+			'name'		=>	'fields['.$key.'][multiple]',
+			'value'		=>	$field['multiple'],
+			'layout'	=>	'horizontal',
+			'message'     => __('Allow multiple?', 'acf-madmimi_audience_list' )
+		));
+
+		?>
+	</td>
+</tr>
+
+		<?php
+
+	}
+
+
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function create_field( $field )
+	{
+		$multiple = ( !empty( $field['multiple'] ) ) ? 'multiple="multiple"' : '';
+		$name = ( !empty( $field['multiple'] ) ) ? esc_attr($field['name']) . '[]' : esc_attr($field['name']);
+
+		echo '<select '.$multiple.' name="'.$name.'">';
+		foreach( $this->madmimi_lists as $list ) {
+			$selected = ( (is_array( $field['value'] ) && in_array( $list['id'], $field['value'] ) ) || ( !is_array( $field['value'] ) && $field['value'] == $list['id'] ) ) ? 'selected="selected"' : '';
+			echo '<option '.$selected.' value="'.$list['id'].'">' . $list['display_name'] . '</option>';
+		}
+		echo '</select>';
+	}
+
+
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add CSS + JavaScript to assist your create_field() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+
+
+		// register ACF scripts
+		wp_register_script( 'acf-input-madmimi_audience_list', $this->settings['dir'] . 'js/input.js', array('acf-input'), $this->settings['version'] );
+		wp_register_style( 'acf-input-madmimi_audience_list', $this->settings['dir'] . 'css/input.css', array('acf-input'), $this->settings['version'] );
+
+
+		// scripts
+		wp_enqueue_script(array(
+			'acf-input-madmimi_audience_list',
+		));
+
+		// styles
+		wp_enqueue_style(array(
+			'acf-input-madmimi_audience_list',
+		));
+
+
+	}
+
+
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your create_field() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add CSS + JavaScript to assist your create_field_options() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add CSS and JavaScript to assist your create_field_options() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  load_value()
+	*
+		*  This filter is applied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value found in the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the value to be saved in the database
+	*/
+
+	function load_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+
+
+	/*
+	*  update_value()
+	*
+	*  This filter is applied to the $value before it is updated in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value which will be saved in the database
+	*  @param	$post_id - the $post_id of which the value will be saved
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the modified value
+	*/
+
+	function update_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+
+
+	/*
+	*  format_value()
+	*
+	*  This filter is applied to the $value after it is loaded from the db and before it is passed to the create_field action
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+
+	function format_value( $value, $post_id, $field )
+	{
+
+				// bail early if no value
+				if( empty($value) ) {
+
+					return $value;
+
+				}
+
+				$value_array = $value;
+				if( !is_array( $value_array ) ) {
+					$value_array = array( $value_array );
+				}
+
+				foreach( $value_array as $i => $value ) {
+
+					if( $field['return'] == 'array' ) {
+						$value_array[$i] = $this->madmimi_lists[$value];
+					}
+					else {
+						$value_array[$i] = $this->madmimi_lists[$value][$field['return']];
+					}
+
+				}
+
+				if( empty( $field['multiple'] ) ) {
+					$value = $value_array[0];
+				}
+				else {
+					$value = $value_array;
+				}
+
+				// return
+				return $value;
+	}
+
+
+	/*
+	*  format_value_for_api()
+	*
+	*  This filter is applied to the $value after it is loaded from the db and before it is passed back to the API functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+
+	function format_value_for_api( $value, $post_id, $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+
+		// perhaps use $field['preview_size'] to alter the $value?
+
+
+		// Note: This function can be removed if not used
+		return $value;
+	}
+
+
+	/*
+	*  load_field()
+	*
+	*  This filter is applied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$field - the field array holding all the field options
+	*/
+
+	function load_field( $field )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+
+
+	/*
+	*  update_field()
+	*
+	*  This filter is applied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*  @param	$post_id - the field group ID (post_type = acf)
+	*
+	*  @return	$field - the modified field
+	*/
+
+	function update_field( $field, $post_id )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+
+
+}
+
+
+// create field
+new acf_field_madmimi_audience_list();
+
+?>
